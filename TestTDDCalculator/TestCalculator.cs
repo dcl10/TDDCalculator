cd9f286770d488909fa5a7b52f0a316c711b0de9
Daniel Lea <Daniel.Lea@nottingham.ac.uk>
2023-04-17 16:47:06 +0100

    fill in unit tests

--- TestTDDCalculator/TestCalculator.cs
+++ TestTDDCalculator/TestCalculator.cs
@@ -1,3 +1,5 @@
+using TDDCalculator;
+
 namespace TestTDDCalculator;
 
 public class TestCalculator
@@ -6,87 +8,116 @@ public class TestCalculator
     public void Calculate_SingleNumber_ReturnsSameNumber()
     {
         // Arrange
+        var formula = "42";
+        var expectedAnswer = 42;
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Equal(expectedAnswer, actualAnswer);
     }
     
     [Fact]
     public void Calculate_AddTwoNumbers_ReturnsSum()
     {
         // Arrange
+        var formula = "40 + 2";
+        var expectedAnswer = 42;
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Equal(expectedAnswer, actualAnswer);
     }
     
     [Fact]
     public void Calculate_SubtractTwoNumbers_ReturnsDifference()
     {
         // Arrange
+        var formula = "44 - 2";
+        var expectedAnswer = 42;
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Equal(expectedAnswer, actualAnswer);
     }
     
     [Fact]
     public void Calculate_MultiplyTwoNumbers_ReturnsProduct()
     {
         // Arrange
+        var formula = "7 * 6";
+        var expectedAnswer = 42;
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Equal(expectedAnswer, actualAnswer);
     }
     
     [Fact]
     public void Calculate_DivideTwoNumbers_ReturnsQuotient()
     {
         // Arrange
+        var formula = "84 / 2";
+        var expectedAnswer = 42;
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Equal(expectedAnswer, actualAnswer);
     }
     
     [Fact]
     public void Calculate_DivideByZero_ThrowsDivideByZeroException()
     {
         // Arrange
+        var formula = "42 / 0";
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = () => calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Throws<DivideByZeroException>(() => actualAnswer);
     }
     
     [Fact]
     public void Calculate_WithNonIntegerArgs_ThrowsArgumentException()
     {
         // Arrange
+        var formula = "42 / abc";
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = () => calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Throws<ArgumentException>(() => actualAnswer);
     }
     
     [Fact]
     public void Calculate_WithInvalidOperator_ThrowsArgumentException()
     {
         // Arrange
+        var formula = "40 & 2";
+        var calculator = new Calculator();
         
         // Act
+        var actualAnswer = () => calculator.Calculate(formula);
         
         // Assert
-        throw new NotImplementedException();
+        Assert.Throws<ArgumentException>(() => actualAnswer);
     }
 }
